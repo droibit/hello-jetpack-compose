64dba6e84055399e685a66323c345ba5319c49b4
Shinya Kumagai <roomful.rooms@gmail.com>
2022-05-23 17:25:44 +0900

    Update development env

--- layouts/src/main/java/com/example/android/hello_compose/MainActivity.kt
+++ layouts/src/main/java/com/example/android/hello_compose/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.android.hello_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import com.example.android.hello_compose.ui.CodelabLayoutsApp
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    WindowCompat.setDecorFitsSystemWindows(window, false)
     setContent {
       CodelabLayoutsApp()
     }
